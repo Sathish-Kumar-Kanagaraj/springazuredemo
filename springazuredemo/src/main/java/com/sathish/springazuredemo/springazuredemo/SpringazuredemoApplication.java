6b7f3a936f2eb1d52dfe6e8921f3c6a7ac9ee8d7
user <user@DESKTOP-TIEP7GJ>
2022-12-07 16:29:59 +0530

    Rest API

--- springazuredemo/src/main/java/com/sathish/springazuredemo/springazuredemo/SpringazuredemoApplication.java
+++ springazuredemo/src/main/java/com/sathish/springazuredemo/springazuredemo/SpringazuredemoApplication.java
@@ -2,12 +2,21 @@ package com.sathish.springazuredemo.springazuredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringazuredemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringazuredemoApplication.class, args);
+	}
+
+	@GetMapping("/messages")
+	public String message() {
+		return "Success Az";
 	}
 
 }
